95bf9afc0341be2e8f3a26b9044310d0359cbaff
Alessandra <alessandra@casamendoza.net>
2024-09-03 08:43:54 -0400

    hw 1

---
+++ homework/ten_mathematical_expressions.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n = 5;
+    int m = 7;
+    cout << "The product of " << n << " and " << m << " is ";
+    cout << n * m << "." << endl;
+    cout << "The sum of " << m << " and " << n << " is " <<n+m<< "." << endl;
+}
