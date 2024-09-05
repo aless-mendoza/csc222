48e8e90364e2ddeeaf94261c2568400ef6019143
Alessandra <alessandra@casamendoza.net>
2024-09-05 09:19:49 -0400

    done

--- homework/c4_e1.cpp
+++ homework/c4_e1.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main() {
+    /*
+    //   1) c
     int i = 4;
     float f = 5;
     bool b1 = i < f, b2 = f < i, b3 = f / i < i / f;
@@ -19,4 +21,24 @@ int main() {
     cout << i << endl;
 
     return 0;
+    */
+    /*
+    //   2) a
+    int a = 2, b = -1, c = 3;
+    if (a > 0) { //true
+        if (b > 0) { //false
+            if (c < 0)
+                c++;
+            if (c <= 0)
+                c++;
+        }
+    if (b < 0) //true
+        a--; //a=1
+    }
+    c = a + b + c; // 3
+    cout << c << endl;
+    */
+    //    3) b
+    int n = 1 % 2 + 4 % 2;
+    cout << n << endl;
 }
