1aa44e87b80c618d2601beff09a3a80290c596ab
Alessandra <alessandra@casamendoza.net>
2024-09-11 08:46:59 -0400

    all practice done

--- homework/c4_e1.cpp
+++ homework/c4_e1.cpp
@@ -43,11 +43,62 @@ int main() {
     //    3) b -> n=1
     int n = 1 % 2 + 4 % 2;
     cout << n << endl;
-    */
-
     // 4 -> 6 
     int x = 3, y = x++, z = ++x;
     cout<<"x = "<<x<<", y = "<<y<<", z = "<<z<<endl;
     cout << (z - x < y - x ? 5 : 6) <<  endl;
     return 0;
-}
+    // 5 -> 1012
+    int n = 2, m = n >> 1, p = m >> n, q = 1 << p, r = q << q;
+    cout << m << p << q << r << endl;
+    return 0;
+    // 6 -> 1120
+    int n = 2, m = n >> 1, p = n >> m, q = 1 << p, r = q >> q;
+    cout << m << p << q << r << endl;
+    return 0;
+    
+    // 7 2911110
+    int x = 0x02, y = 011, z = x ^ y, u = z | x, v = u & 0;
+    cout << x << y << z  << u << v << endl;
+    return 0;
+    
+    // 8 36
+    int n = (3 % 4 + 6) << 2;
+    cout << n << endl;
+    return 0;
+    
+    // 9
+    int n = 3 << 2 + 4 >> 1;
+    cout<<n<<endl;
+
+    //10 671
+    char a = 3 << 1, b = a | 3, c = a ^ b;
+    cout << a << b << c << endl;
+    cout << int(a) << int(b) << int(c) << endl;
+    return 0;
+    
+    //11 158
+    unsigned char n = 'a', m = ~n;
+    cout << int(m) << endl;
+    return 0;
+    
+    //12 "no"
+    unsigned char n = 6, m = 9;
+    cout << (n & m ? "yes" : "no") << endl;
+    unsigned char x = 15, y = 15;
+    cout << (x & y ? "yes" : "no") << endl;
+    return 0;
+    */
+    //13 mustard and ice cream
+    bool b1 = true, b2 = false, b3 = !b2;
+    cout << (b1 || b2 ? "mustard" : "ketchup");
+    cout << " and ";
+    if (b3)
+        if (b1 && !(b2 && b3))
+            cout << "ice cream";
+        else
+            cout << "spinach";
+    cout << endl;
+    return 0;
+}   
+
