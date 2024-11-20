3d3d6fe1cedcd9c575a6372980d24db3164f0a95
Alessandra <alessandra@casamendoza.net>
2024-11-20 13:32:14 -0500

    oo notes

--- homework/notes/c11_ex.cpp
+++ homework/notes/c11_ex.cpp
@@ -8,6 +8,7 @@ struct Time {
     void print();
     void increment(double secs);
     double convert_to_seconds() const;
+    bool after(const &Time t2) const;
 };
 
 void Time::print()
@@ -34,11 +35,26 @@ double Time::convert_to_seconds() const
     return (hour * 60 + minute) * 60 + second;
 }
 
+bool Time::after(const &Time t2) const
+{
+    if (hour > t2.hour) return true;
+    if (hour < t2.hour) return false;
+
+    if (minute > t2.minute) return true;
+    if (minute < t2.minute) return false;
+
+    return (second > t2.second);
+}
+
 int main(){
     Time current_time = {9, 14, 30.0};
     current_time.increment(500.0);
     current_time.print();
     current_time.convert_to_seconds();
+    if (done_time.after(current_time)) {
+        cout << "The bread will be done after it starts." << endl;
+    }
+
 
     return 0;
 }
---
+++ homework/notes/essonce_of_oo.md
@@ -0,0 +1,13 @@
+# Video Notes
+
+## 1) What is code rot, and why does it happen?
+
+Code rot is when the quality of the code (structure) gets worse over time. The reason this happens is that the programmer becomes lazy or has a tight deadline and takes shortcuts in the code. Code rot makes code hard to read and follow which makes trouble shooting and bug fixes difficult 
+
+## 2) What is the design of a software product?
+
+The design of a software product is source code that the designer makes for the project.
+
+## 3) According to Uncle Bob, what is the essential characteristic of Object-Oriented design?
+
+The essential characteristice of Object-Oriented design is its flexibility.
